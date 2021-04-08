e1d1591d7a1a5a091b79141646c6b9efa3f6f380
asmartin <asmartin@iastate.edu>
2021-04-08 12:16:34 -0500

    bedZero and rollerZero should have the proper commands for resetting the powderbed and the 3-axis motor to the absolute zero position.

--- gcode_to_vxm/pbed/bedZero.m
+++ gcode_to_vxm/pbed/bedZero.m
@@ -1,11 +1,10 @@
 % Resets the powderbed to absolute zero by moving the motors to it's limits.
 % F = On-Line mode with echo off
 % C = clear all commands from current program
-% S2M 600 = Set Motor 2 Speed 600 steps/second
-% S3M 600 = Set Motor 3 Speed 600 steps/second
-% (I3M0, I2M0,) = Index motor 2 and 3 simultaneously
-% IA2M-0, IA3M-0 = Set Zero position for motor 2 and 3
+% S1M 600 = Set Motor 1 Speed 600 steps/second
+% I1M0 = Index motor 1
+% IA1M-0 = Set Zero position for motor 1
 % R = run command
 function bedCMD = bedZero()
-    bedCMD = 'F, C, S1M600, (I2M0, I3M0), IA2M-0, IA3M-0  R';
+    bedCMD = 'F, C, S1M600, I1M0, IA1M-0, R';
 end
