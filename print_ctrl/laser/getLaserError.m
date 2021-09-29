5038f67d9a9e1b3512144563cb0f02de263e3fa4
asmartin <asmartin@iastate.edu>
2021-09-29 14:48:51 -0500

    getLaserError.m  see Customer Command Reference

---
+++ print_ctrl/laser/getLaserError.m
@@ -0,0 +1,28 @@
+function res = getLaserError(val)
+
+    switch val
+    case 0x00
+        res = "Command not supported";
+    case 0x01
+        res = "CRC error";
+    case 0x02
+        res = "No Start byte";
+    case 0x03
+        res = "No Stop byte";
+    case 0x04
+        res = "Incorrect No Data Bytes";
+    case 0x05
+        res = "Overrun error";
+    case 0x06
+        res = "Parity error";
+    case 0x07
+        res = "Framing error";
+    case 0x08
+        res = "Rx buffer overflow";
+    case 0x09
+        res = "Command time out";
+    otherwise
+        res = "Unknown Error";
+    end
+    
+end
