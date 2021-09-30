abf9404ff3a86726886ce1af4b88a6176a5e3755
asmartin <asmartin@iastate.edu>
2021-09-29 19:33:07 -0500

    Tary's printerHome moved to utils. homeAxisRoller and homeBeds are similar, but designed for compiler use. Added ZERO_POS global const struct for our defined zero positions.

--- print_ctrl/utils/VXM_MOTOR_MAP.m
+++ print_ctrl/utils/VXM_MOTOR_MAP.m
@@ -6,18 +6,18 @@
 % To use, place near the start of your function like so
 %   map = VXM_MOTOR_MAP
 
-function map = VXM_MOTOR_MAP() 
-
-    map.port_a = "COM5";  % Maps to motors 1-4
-    map.port_b = "COM11"; % Maps to motors 5 and 6
+function map = VXM_MOTOR_MAP()
 
     % Motor Indexing Map:
-    % MANUALLY CHANGE IF NECESSARY
+    % Our index = vxm index
+    % MANUALLY CHANGE IF NEEDED
+    map.port_a = "COM5";  % Maps to motors 1-4
     map.m1 = 1; % Spotsize
     map.m2 = 4; % Roller
     map.m3 = 2; % X-axis movement (left/right)
     map.m4 = 3; % Y-axis movement (forward/back)
 
+    map.port_b = "COM11"; % Maps to motors 5 & 6
     map.m5 = 1; % Supply Bed
     map.m6 = 2; % Powder Bed
 
