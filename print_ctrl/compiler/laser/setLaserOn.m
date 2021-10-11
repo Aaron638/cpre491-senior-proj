dad493ae324e98270f976a13e96b6cb76c0debeb
asmartin <asmartin@iastate.edu>
2021-10-11 00:14:45 -0500

    Modified laser on and off commands to return targeting laser commands for testing. Created a laserCMDs.json for developing a command dictionary where programmers can quickly write input commands, and parse results.

--- print_ctrl/compiler/laser/setLaserOn.m
+++ print_ctrl/compiler/laser/setLaserOn.m
@@ -1,5 +1,7 @@
 % M201: Laser On
+% Does targeting laser for now
 function laserCMD = setLaserOn()
-    laserCMD = "LASER_ON";
-    % laserCMD = getLaserStatus
+    laserCMD = [0x1B, 0x02, 0x10, 0x01, 0x0D];
+    laserCMD = [laserCMD, sum(laserCMD)];   % CRC byte
+    % compose("0x%02X", setLaserOn) To view
 end
