8f8dc659a10799c22e2ea83d961c90139a473db3
cpf6692 <cpf6692@iastate.edu>
2021-11-04 20:27:08 -0500

    Added multi defect functionality to slicer_ctrl() file.

--- stl_to_gcode/Slicer/slicer_ctrl.m
+++ stl_to_gcode/Slicer/slicer_ctrl.m
@@ -14,17 +14,23 @@ function slicer_ctrl(config_file)
     voxels_per_width = data.voxels_per_width;
     voxel_margin = data.voxel_margin;
     voxel_padding = data.voxel_padding;
-    defect_x_origin = data.defect_x_origin;
-    defect_y_origin = data.defect_y_origin;
-    defect_z_origin = data.defect_z_origin;
-    defect_width = data.defect_width;
-    defect_length = data.defect_length;
-    defect_height = data.defect_height;
-
     gen_cube(length, width, height, layer_height, voxels_per_length, voxels_per_width, voxel_margin, voxel_padding);
 
-    if defect_x_origin ~= -1 && defect_y_origin ~= -1 && defect_z_origin ~= -1 && defect_width ~= -1 && defect_length ~= -1 && defect_height ~= -1         
-        insert_defect('test.gcode', defect_x_origin, defect_y_origin, defect_z_origin, defect_width, defect_length, defect_height, length, width, height);
-    end
+    for n_defects=1:size(data.defects,1)
+
+        n_array = data.defects(n_defects,1);
+        defect_x_origin = n_array.defect_x_origin;
+        defect_y_origin =  n_array.defect_y_origin;
+        defect_z_origin =  n_array.defect_z_origin;
+        defect_width =  n_array.defect_width;
+        defect_length =  n_array.defect_length;
+        defect_height =  n_array.defect_height;
+   
+        if defect_x_origin ~= -1 && defect_y_origin ~= -1 && defect_z_origin ~= -1 && defect_width ~= -1 && defect_length ~= -1 && defect_height ~= -1 
+            insert_defect('test.gcode', defect_x_origin, defect_y_origin, defect_z_origin, defect_width, defect_length, defect_height, length, width, height);
+        end
+
+
+     end
 
 end
