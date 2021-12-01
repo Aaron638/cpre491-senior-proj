47f39e911337aefecd00a6c0f69f8325c615ba41
asmartin <asmartin@iastate.edu>
2021-12-01 14:53:11 -0600

    Made a safter stopMotors command. Untested.

--- printCtrl/sender/motor/killMotors.m
+++ printCtrl/sender/motor/killMotors.m
@@ -1,4 +1,5 @@
 % killMotors
+% Interrupts running motor commands.
 % Sends a kill command "K" to both serial ports connected to the VXM motors.
 % Can potentially cause problems if motorspeed > 1000 steps/second
 % See VXM-2 Users Manual for details
@@ -8,12 +9,17 @@ function killMotors(serialTwin, serialSolo)
     write(serialSolo, cmd, "uint8");
     disp("Sent kill command.");
 
-    % Ensures that we encounter a response before flushing and deleting the device.
+    % Trys to get a response before flushing and deleting the device.
     try
-        read(serialTwin, 1, "uint8");
-        read(serialSolo, 1, "uint8");
+        rT = read(serialTwin, 1, "uint8");
+        rS = read(serialSolo, 1, "uint8");
     catch
-        warning("WARN: Did not receieve response from kill command.");
+        if rT ~= '^'
+            warning("WARN: Did not receieve '^' response from twin motors.");
+        end
+        if rS ~= '^'
+            warning("WARN: Did not receieve '^' response from solo motors.");
+        end
     end
 
     flush(serialTwin); flush(serialSolo);
